20c061ef533003271bd28d96ad855741c4feb0e2
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 21:17:07 -0500

    Excess Report View enabled

--- src/Manager/OpenExcessReportWindow.java
+++ src/Manager/OpenExcessReportWindow.java
@@ -1,7 +1,32 @@
 package src.Manager;
 
-public class OpenExcessReportWindow {
+import src.Database.jdbcpostgreSQL;
+
+import javax.swing.*;
+
+public class OpenExcessReportWindow extends JFrame {
+
+    String date;
     OpenExcessReportWindow(String dateInput){
 
+        date = dateInput;
+        // Data to be displayed in the JTable
+        Object[][] data;
+        String[][] invTable = new String[200][4];
+        //jdbcpostgreSQL.getSalesReportTable(invTable, date);
+        data = invTable;
+        // Column Names
+        String[] columnNames = {"ingredients", "amount used", "current amount", "units"};
+
+        JTable salesReport = new JTable(data, columnNames);
+        salesReport.setFillsViewportHeight(true);
+
+        // // adding it to JScrollPane
+        JScrollPane sp = new JScrollPane(salesReport);
+
+        this.setTitle("Excess Report");
+        this.add(sp);
+        this.pack();
+        this.setVisible(true);
     }
 }
